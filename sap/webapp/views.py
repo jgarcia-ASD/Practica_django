86a86762cfacd57dcb74c015237df1eb31c2ac4f
jgarcia-ASD <jgarcia@grupoasd.com>
2023-07-14 11:49:44 -0500

    terminado seccion 77 y 76 curso udemy

---
+++ sap/webapp/views.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+def bienvenido(request):
+    return HttpResponse('Hola mundo desde Django')
+
+def despedirse(request):
+    return HttpResponse('Despedida desde Django')
+
+def contacto(request):
+    return HttpResponse('Email: jgarcia@grupoasd.com Celular: +57 3213019066')
