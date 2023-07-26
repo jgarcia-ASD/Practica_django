bdfe83fd164964ccf7f9ee59e26638c22ff05dcc
jgarcia-ASD <jgarcia@grupoasd.com>
2023-07-26 09:36:54 -0500

    video 1:45:45 Listar tareas

--- README.md
+++ README.md
@@ -7,11 +7,19 @@ esta rama esta hecha con apoyo de un video de youtube el cual la idea es impleme
 ### futuras explicaciones de cambios
 #### ahorrar codigo
 podemos implementar un import para importar todas las vistas y no añadir una por una esto eesta en urls
-```
+```python
 from task import tasks
 ```
 #### crear usuarios 
 django ya ofrece esto si usamos lo siguiente en el archivo views de nuestra app
-```
+```python
 from django.contrib.auth.forms import UserCreationForm
+```
+
+#### podemos tener una base html 
+para usarlo como plantilla  asi otras paginas heredan lo que tengamos en nuestra pantilla principal
+```python
+{% block content %}
+
+{% endblock %}
 ```
--- tasks/views.py
+++ tasks/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import redirect, render
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
-from django.contrib.auth import login
+from django.contrib.auth import login, logout, authenticate
 from django.db import IntegrityError
+from .forms import TaskForm
+from .models import Task
 
 # Create your views here.
 def home(request):
@@ -34,4 +36,46 @@ def signup(request):
     })
 
 def tasks(request):
-    return render(request, 'tasks.html')
+    tasks = Task.objects.filter(user=request.user, datecompleted__isnull=True)
+    return render(request, 'tasks.html', {
+        'tasks': tasks
+    })
+
+def createtasks(request):
+
+    if request.method == 'GET':
+        return render(request, 'created_task.html', {
+            'form': TaskForm
+        })
+    else: 
+        try:
+            form = TaskForm(request.POST)
+            new_tasks = form.save(commit=False)
+            new_tasks.user = request.user
+            new_tasks.save()
+            return redirect ('tasks')
+        except ValueError:
+            return render(request, 'created_task.html', {
+                'form': TaskForm,
+                'error': 'no se pudo crear la tarea'
+            })
+
+def signout(request):
+    logout(request)
+    return redirect ('home')
+
+def signin(request):
+    if request.method == 'GET':
+        return render(request, 'signin.html', {
+            'form' : AuthenticationForm
+        })
+    else:
+        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            return render(request,'signin.html', {
+                'form' : AuthenticationForm,
+                'error': 'usuario o contraseña incorrectos'
+            })
+        else:
+            login(request, user)
+            return redirect('tasks')
