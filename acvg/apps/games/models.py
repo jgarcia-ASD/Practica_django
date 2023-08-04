450928c4e027467e69c3156548b06801511873dd
jgarcia-ASD <jgarcia@grupoasd.com>
2023-08-04 09:07:31 -0500

    Configuracion conexion Bd mysql | Finalizado

--- acvg/apps/games/models.py
+++ acvg/apps/games/models.py
@@ -4,6 +4,9 @@ from django.db import models
 class Country(models.Model):
     country = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.country
+
 class Author(models.Model):
     documento = models.IntegerField(primary_key=True)
     nickname = models.CharField(max_length=100)
@@ -12,5 +15,8 @@ class Author(models.Model):
     email = models.EmailField()
     nationality = models.ForeignKey(Country, on_delete=models.SET_NULL, null=True)
     descripcion = models.TextField(blank = False)
+
+    def __str__(self):
+        return self.nickname
 
 
